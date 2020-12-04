e75d484805133e633cdda79e766fe7bc0545b9aa
PetePrattis <panagiotispr1997@gmail.com>
2020-12-04 22:13:57 +0200

    Initial commit

---
+++ src/main/java/com/codehub/projectfuture/team3/PropertyRepairWebApp/PropertyRepairWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.codehub.projectfuture.team3.PropertyRepairWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyRepairWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyRepairWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codehub/projectfuture/team3/PropertyRepairWebApp/PropertyRepairWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehub.projectfuture.team3.PropertyRepairWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyRepairWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
