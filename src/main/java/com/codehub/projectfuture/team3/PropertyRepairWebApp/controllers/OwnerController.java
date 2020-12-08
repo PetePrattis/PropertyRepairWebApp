42b0601e431171f73141437a07bafc7e8a77864d
Petridis Christos <xirtsospetridis@gmail.com>
2020-12-08 14:50:44 +0200

    added basic View design for entities

--- src/main/java/com/codehub/projectfuture/team3/PropertyRepairWebApp/controllers/OwnerController.java
+++ src/main/java/com/codehub/projectfuture/team3/PropertyRepairWebApp/controllers/OwnerController.java
@@ -6,14 +6,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class OwnerController {
     @Autowired
     private OwnerService ownerService;
 
+    @GetMapping({"/", "/login"})
+    public String ownerLoginView(Model model) {
+        Owner owner = ownerService.findOwnerById(1L).orElseThrow();
+
+        model.addAttribute("anOwner", owner);
+        return "Index";
+    }
+
+    @GetMapping("/admin")
+    public String adminHomePageView(Model model) {
+        Owner owner = ownerService.findOwnerById(1L).orElseThrow();
+
+        model.addAttribute("anOwner", owner);
+        return "Index";
+    }
+
     @GetMapping("/owner")
-    public String ownerView(Model model){
+    public String ownerHomePageView(Model model) {
+        Owner owner = ownerService.findOwnerById(1L).orElseThrow();
+
+        model.addAttribute("anOwner", owner);
+        return "Index";
+    }
+
+    @PostMapping("/create-owner")
+    public String createOwnerView(Model model) {
+        Owner owner = ownerService.findOwnerById(1L).orElseThrow();
+
+        model.addAttribute("anOwner", owner);
+        return "Index";
+    }
+
+    @PostMapping("/edit-owner")
+    public String editOwnerView(Model model) {
+        Owner owner = ownerService.findOwnerById(1L).orElseThrow();
+
+        model.addAttribute("anOwner", owner);
+        return "Index";
+    }
+
+    @PostMapping("/search-owner")
+    public String searchOwnerView(Model model) {
         Owner owner = ownerService.findOwnerById(1L).orElseThrow();
 
         model.addAttribute("anOwner", owner);
--- src/main/java/com/codehub/projectfuture/team3/PropertyRepairWebApp/controllers/RepairController.java
+++ src/main/java/com/codehub/projectfuture/team3/PropertyRepairWebApp/controllers/RepairController.java
@@ -1,11 +1,13 @@
 package com.codehub.projectfuture.team3.PropertyRepairWebApp.controllers;
 
+import com.codehub.projectfuture.team3.PropertyRepairWebApp.domains.Owner;
 import com.codehub.projectfuture.team3.PropertyRepairWebApp.domains.Repair;
 import com.codehub.projectfuture.team3.PropertyRepairWebApp.services.RepairService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -20,5 +22,20 @@ public class RepairController {
 
         model.addAttribute("aRepair", repairList.get(0));
         return "Index2";
+    }
+
+    @PostMapping("/create-repair")
+    public String createRepairView(Model model) {
+        return "Index";
+    }
+
+    @PostMapping("/edit-repair")
+    public String editRepairView(Model model) {
+        return "Index";
+    }
+
+    @PostMapping("/search-repair")
+    public String searchRepairView(Model model) {
+        return "Index";
     }
 }
