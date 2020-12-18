6281c976ed6257217ec4b1132f5f75f6c0eff749
Michael Panagiotou <michalespan@gmail.com>
2020-12-18 22:42:28 +0200

    Complete validation for create Owner form

--- src/main/java/com/codehub/projectfuture/team3/PropertyRepairWebApp/controllers/owner/CreateOwnerController.java
+++ src/main/java/com/codehub/projectfuture/team3/PropertyRepairWebApp/controllers/owner/CreateOwnerController.java
@@ -1,16 +1,22 @@
 package com.codehub.projectfuture.team3.PropertyRepairWebApp.controllers.owner;
 
+import com.codehub.projectfuture.team3.PropertyRepairWebApp.domains.Owner;
 import com.codehub.projectfuture.team3.PropertyRepairWebApp.enums.PropertyType;
 import com.codehub.projectfuture.team3.PropertyRepairWebApp.enums.UserRole;
 import com.codehub.projectfuture.team3.PropertyRepairWebApp.forms.OwnerForm;
+import com.codehub.projectfuture.team3.PropertyRepairWebApp.model.OwnerModel;
 import com.codehub.projectfuture.team3.PropertyRepairWebApp.services.OwnerService;
+import com.codehub.projectfuture.team3.PropertyRepairWebApp.validators.OwnerFormValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -19,9 +25,19 @@ public class CreateOwnerController {
     private static final String OWNERS_FORM = "ownersForm";
     private static final String PROPERTY_TYPES = "propertyType";
     private static final String USER_ROLE = "userRole";
+    private static final String ERROR_MESSAGE = "errorMessage";
 
     @Autowired
     private OwnerService ownerService;
+
+    @Autowired
+    private OwnerFormValidation ownerFormValidation;
+
+
+    @InitBinder(OWNERS_FORM)
+    protected void initBinder(final WebDataBinder binder) {
+        binder.addValidators(ownerFormValidation);
+    }
 
     @GetMapping(value = "/admin/owners/create")
     public String createOwnerView(Model model) {
@@ -33,12 +49,17 @@ public class CreateOwnerController {
 
     @PostMapping(value = "/admin/owners/create")
     public String createOwner(Model model, @Valid @ModelAttribute(OWNERS_FORM) OwnerForm ownerForm,
-                              BindingResult bindingResult) {
+                              BindingResult bindingResult,
+                              RedirectAttributes redirectAttributes) {
 
         if (bindingResult.hasErrors()) {
-            return "/admin/owners/create";
+            model.addAttribute(PROPERTY_TYPES, PropertyType.values());
+            model.addAttribute(USER_ROLE, UserRole.values());
+            model.addAttribute(ERROR_MESSAGE, "validation errors occurred");
+            return "pages/owner/owner_create";
         }
-        ownerService.createOwner(ownerForm);
+        OwnerModel owner = ownerService.createOwner(ownerForm);
+        redirectAttributes.addAttribute("id",owner.getId());
         return "redirect:/admin/owners";
     }
 }
---
+++ src/main/java/com/codehub/projectfuture/team3/PropertyRepairWebApp/validators/OwnerFormValidation.java
@@ -0,0 +1,48 @@
+package com.codehub.projectfuture.team3.PropertyRepairWebApp.validators;
+
+import com.codehub.projectfuture.team3.PropertyRepairWebApp.forms.OwnerForm;
+import com.codehub.projectfuture.team3.PropertyRepairWebApp.model.OwnerModel;
+import com.codehub.projectfuture.team3.PropertyRepairWebApp.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class OwnerFormValidation implements Validator {
+    private static final String AFM_PATTERN = "[0-9]{9}";
+
+    @Autowired
+    private OwnerService ownerService;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return OwnerForm.class.isAssignableFrom(aClass);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        OwnerForm registrationForm = (OwnerForm) target;
+        // Here we add our custom validation logic
+        if(registrationForm.getAfm().matches(AFM_PATTERN))
+        {
+            Optional<OwnerModel> ownerByAfm = ownerService.findOwnerByAfm(Long.valueOf(registrationForm.getAfm()));
+            if (ownerByAfm.isPresent()) {
+                errors.rejectValue("afm", "register.afm.taken.error");
+            }
+        }
+        Optional<OwnerModel> ownerByEmail = ownerService.findOwnerByEmail(registrationForm.getEmail());
+        if (ownerByEmail.isPresent()) {
+            errors.rejectValue("email", "register.email.taken.error");
+        }
+        // Or use reject if empty or whitespace
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "register.not.null.or.empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "register.not.null.or.empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address", "register.not.null.or.empty");
+    }
+}
+
