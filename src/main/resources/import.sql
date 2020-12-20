f9d9532574a4cb52bb0dba782cabf87b154f54b0
Michael Panagiotou <michalespan@gmail.com>
2020-12-20 19:38:27 +0200

    Add owner views

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -22,15 +22,17 @@ INSERT INTO OWNER (owner_AFM, firstname, lastname, owner_address, owner_telephon
 
 INSERT INTO PROPERTY (property_code,property_address,property_constructionYear,property_type,owner_id) VALUES ('PROPERTY1','Address 1', 2021,'HOUSE',1)
 INSERT INTO PROPERTY (property_code,property_address,property_constructionYear,property_type,owner_id) VALUES ('PROPERTY2','Address 2', 2021,'HOUSE',1)
-INSERT INTO PROPERTY (property_code,property_address,property_constructionYear,property_type,owner_id) VALUES ('PROPERTY3','Address 3', 2021,'HOUSE',1)
-INSERT INTO PROPERTY (property_code,property_address,property_constructionYear,property_type,owner_id) VALUES ('PROPERTY4','Address 4', 2021,'HOUSE',1)
-INSERT INTO PROPERTY (property_code,property_address,property_constructionYear,property_type,owner_id) VALUES ('PROPERTY5','Address 5', 2021,'HOUSE',1)
+INSERT INTO PROPERTY (property_code,property_address,property_constructionYear,property_type,owner_id) VALUES ('PROPERTY3','Address 3', 2021,'HOUSE',2)
+INSERT INTO PROPERTY (property_code,property_address,property_constructionYear,property_type,owner_id) VALUES ('PROPERTY4','Address 4', 2021,'HOUSE',2)
+INSERT INTO PROPERTY (property_code,property_address,property_constructionYear,property_type,owner_id) VALUES ('PROPERTY5','Address 5', 2021,'HOUSE',3)
 
 INSERT INTO REPAIR (repair_address,repair_cost,repair_date,extra_info,repair_status,repair_type,owner_id,property_id) VALUES ('Address 1',2000.14,'2021-01-11','Extra info 1','INPROGRESS','INSULATION',1,1)
 INSERT INTO REPAIR (repair_address,repair_cost,repair_date,extra_info,repair_status,repair_type,owner_id,property_id) VALUES ('Address 2',200021.14,'2021-02-11','Extra info 2','INPROGRESS','PAINTING',2,2)
 INSERT INTO REPAIR (repair_address,repair_cost,repair_date,extra_info,repair_status,repair_type,owner_id,property_id) VALUES ('Address 3',20100.14,'2021-01-11','Extra info 3','INPROGRESS','PLUMBING',3,3)
 INSERT INTO REPAIR (repair_address,repair_cost,repair_date,extra_info,repair_status,repair_type,owner_id,property_id) VALUES ('Address 4',20300.14,'2021-01-11','Extra info 4','INPROGRESS','PLUMBING',1,4)
 INSERT INTO REPAIR (repair_address,repair_cost,repair_date,extra_info,repair_status,repair_type,owner_id,property_id) VALUES ('Address 5',21300.14,'2021-11-11','Extra info 5','PENDING','PLUMBING',1,4)
+INSERT INTO REPAIR (repair_address,repair_cost,repair_date,extra_info,repair_status,repair_type,owner_id,property_id) VALUES ('Address test',200,'2021-11-10','Extra info test','PENDING','PLUMBING',2,4)
+INSERT INTO REPAIR (repair_address,repair_cost,repair_date,extra_info,repair_status,repair_type,owner_id,property_id) VALUES ('Address test1',2130,'2021-11-12','Extra info test1','INPROGRESS','PLUMBING',2,4)
 
 -- insert into BOOK(book_id, book_title,publication_year,author_id,book_category) values ('fbe25e54-645f-4ede-829b-d28f3a29b2dc','War and Peace','1867','1','LITERARY_REALISM');
 -- insert into BOOK(book_id, book_title,publication_year,author_id,book_category) values ('a6fe1574-04de-44d6-ae2c-9f43fc2f42f1','The Grapes of Wrath','1939','2','DRAMA');
